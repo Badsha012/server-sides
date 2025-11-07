c40b98ac0fc4c81029bdce9ae19500fa4f47aa17
Badsha012 <badshasheikh361@gmail.com>
2025-11-07 09:25:38 +0600

    added a mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const cors=require('cors')
+const express = require('express');
+const cors=require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = 3000
 
@@ -19,10 +21,9 @@ app.get('/', (req, res) => {
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.cyspe14.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://modal-db:EGkaoWQfwP3sH7fR@cluster0.cyspe14.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,14 +34,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+  
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
+  
   }
 }
 run().catch(console.dir);
